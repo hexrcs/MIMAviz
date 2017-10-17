5eb76a0568fcb24e00389c0d60845fbb88fde6ce
Xiaoru Li <thexrcs@gmail.com>
2017-10-17 16:28:16 +0200

    Make ioArrowHeadDrawer more concise

--- src/js/view/drawers/ioArrowHeadDrawer.js
+++ src/js/view/drawers/ioArrowHeadDrawer.js
@@ -2,14 +2,13 @@ import * as PIXI from "pixi.js";
 import {default as h} from "../../helpers";
 
 export default function ioArrowHeadDrawer(arrowHead=[], alt=false) {
-  const [x1,y1, x2,y2, x3,y3] = arrowHead;
   let sprite = new PIXI.Graphics();
   if(!alt) {
     sprite.beginFill(h.colorCode.BORDER);
   } else {
     sprite.beginFill(h.colorCode.INSIDE_ALT);
   }
-  sprite.drawPolygon([x1,y1, x2,y2, x3,y3]);
+  sprite.drawPolygon(arrowHead);
   
   return sprite;
 }
