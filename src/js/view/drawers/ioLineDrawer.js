ad1449d0f4bc917fcc30d78db59bcdf441c5a8d4
Xiaoru Li <thexrcs@gmail.com>
2017-10-17 10:22:49 +0200

    Implement ioLineDrawer function

---
+++ src/js/view/drawers/ioLineDrawer.js
@@ -0,0 +1,25 @@
+import * as PIXI from "pixi.js";
+import {default as h} from "../../helpers";
+
+export default function ioLineDrawer(dir="h", x=0, y=0, length=0, alt=false) {
+  let sprite = new PIXI.Graphics();
+  if(!alt) {
+    sprite.lineStyle(4, h.colorCode.BORDER);
+  } else {
+    sprite.lineStyle(4, h.colorCode.INSIDE_ALT);
+  }
+
+  let [dx, dy] = [x,y];
+  switch(dir) {
+    case "h":
+      dy += length;
+      break;
+    case "v":
+      dx += length;
+      break;
+  }
+  sprite.moveTo(x, y);
+  sprite.lineTo(dx, dy);
+
+  return sprite;
+}
