187dba0ce478e8b283b988a855576e0d8712dcf1
Xiaoru Li <thexrcs@gmail.com>
2017-10-17 16:54:57 +0200

    Implement cuPhaseBGDrawer function

---
+++ src/js/view/drawers/cuPhaseBGDrawer.js
@@ -0,0 +1,17 @@
+import * as PIXI from "pixi.js";
+import {default as h} from "../../helpers";
+
+export default function cuPhaseBGDrawer(name="") {
+  let sprite = new PIXI.Graphics();
+  sprite.beginFill(h.colorCode.LIGHT_BG);
+  switch(name) {
+    case "fetch":
+      sprite.drawRect(540,90, 240, 20);
+      break;
+    case "exec":
+      sprite.drawRect(540,240, 240, 20);
+      break;
+  }
+  
+  return sprite;
+}
