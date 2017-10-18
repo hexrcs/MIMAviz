b50f402a47f218cba3a7fe161b0985e12ba69c87
Xiaoru Li <thexrcs@gmail.com>
2017-10-18 16:22:57 +0200

    Implement ioPathOrganizer

---
+++ src/js/view/organizers/ioPathOrganizer.js
@@ -0,0 +1,68 @@
+import * as PIXI from "pixi.js";
+import {default as h} from "../../helpers";
+import {ioCellArrowBuilder, ioBusBuilder} from "../builders";
+
+export default function ioPathOrganizer(
+  state={global: {mode: "IDLE", step: 0}}
+) {
+  let container = new PIXI.Container();
+  const path = h.jsonParser.processInterpreter(state)["path"];
+  const cellsFrom = new Set();
+  const cellsTo = new Set();
+
+  // cell arrows
+  for(let p of path) {
+    cellsFrom.add(p["from"]);
+    cellsTo.add(p["to"]);
+  }
+  for(let c of ["Akku", "Eins", "X", "Y", "Z", "SAR", "IAR",  "IR", "SDR"]) {
+    if(cellsFrom.has(c)) {
+      container.addChild(ioCellArrowBuilder(c, whichArrows(c, true), true));
+    } else if(cellsTo.has(c)) {
+      container.addChild(ioCellArrowBuilder(c, whichArrows(c, false, true), true));
+    } else {
+      container.addChild(ioCellArrowBuilder(c, whichArrows(c)));
+    }
+  }
+
+  // main bus
+  for(let p of path) {
+    container.addChild(ioBusBuilder(p["from"], p["to"]));
+  }
+  
+  return container;
+}
+
+function whichArrows(name="", from=false, to=false) {
+  switch(cellName) {
+    case "Akku":
+      if(to) {
+        return "l";
+      }
+      if(from) {
+        return "r";
+      }
+      if(!(from || to)) {
+        return "lr";
+      }
+    case "Eins":
+    case "X":
+    case "Y":
+      return "r";
+    case "Z":
+    case "SAR":
+      return "l";
+    case "IAR":
+    case "IR":
+    case "SDR":
+      if(to) {
+        return "l";
+      }
+      if(from) {
+        return "r";
+      }
+      if(!(from || to)) {
+        return "lr";
+      }
+  }
+}
