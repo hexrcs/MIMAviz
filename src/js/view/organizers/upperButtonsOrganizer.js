e3b8027e0bed434b357affabc29da448a753665d
Xiaoru Li <thexrcs@gmail.com>
2017-10-18 16:53:52 +0200

    Attempt to implement upperButtonsOrganizer
    Too tired today to think straight... This should be it for today.
    Will get all the actions firing thought through tomorrow.

---
+++ src/js/view/organizers/upperButtonsOrganizer.js
@@ -0,0 +1,24 @@
+import * as PIXI from "pixi.js";
+import {default as h} from "../../helpers";
+import {buttonBuilder} from "../builders";
+
+export default function upperButtonsOrganizer(
+  state={
+    global: {mode: "IDLE", step: 0},
+    upperButton: {alt: "", status: 0},
+  }
+) {
+  const container = new PIXI.Container();
+
+  for(let b of ["ADD", "LDC", "LDV", "NOT", "STV"]) {
+    if(b === state.upperButton.alt) {
+      const button = buttonBuilder(b, state.upperButton.status);
+      container.addChild(button);
+    } else {
+      const button = buttonBuilder(b);
+      container.addChild(button);
+    }
+  }
+
+  return container;
+}
