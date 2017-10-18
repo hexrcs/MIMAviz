9c44ac2a37cf61ef0ebbc83d01af86ee79ab4550
Xiaoru Li <thexrcs@gmail.com>
2017-10-18 11:13:23 +0200

    Implement descriptionOrganizer

---
+++ src/js/view/organizers/descriptionOrganizer.js
@@ -0,0 +1,16 @@
+import * as PIXI from "pixi.js";
+import {default as h} from "../../helpers";
+import {default as bottomDescriptionBuilder} from "../builders/bottomDescriptionBuilder";
+import {default as cuDescriptionBuilder} from "../builders/cuDescriptionBuilder";
+
+export default function descriptionOrganizer(
+  state={global: {mode: "IDLE", step: 0}}
+) {
+  let container = new PIXI.Container();
+  const text = h.jsonParser.descriptionInterpreter(state);
+  const bottomSprite = bottomDescriptionBuilder(text.bottomDescription);
+  const cuSprite = cuDescriptionBuilder(text.cuDescription);
+
+  container.addChild(bottomSprite, cuSprite);
+  return container;
+}
