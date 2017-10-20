f5f6f62e8bdcd35347d26b712a7f6705e83f047b
Xiaoru Li <thexrcs@gmail.com>
2017-10-20 21:06:14 +0200

    Simplify JSON structure

--- src/js/helpers/jsonParser.js
+++ src/js/helpers/jsonParser.js
@@ -6,14 +6,14 @@ function processInterpreter (state = {mode: '', step: 0}) {
   const mode = state.mode;
   const step = state.step;
 
-  return pd[mode]['step'][step];
+  return pd[mode][step];
 }
 
 function descriptionInterpreter (state = {mode: '', step: 0}) {
   const mode = state.mode;
   const step = state.step;
 
-  return d[mode]['step'][step];
+  return d[mode][step];
 }
 
 function cuDisplayInterpreter (state = {mode: '', step: 0}) {
@@ -25,7 +25,7 @@ function cuDisplayInterpreter (state = {mode: '', step: 0}) {
 function totalStepCount (state = {mode: '', step: 0}) {
   const mode = state.mode;
 
-  return pd[mode]['step'].length;
+  return pd[mode].length;
 }
 
 export default {processInterpreter, descriptionInterpreter, cuDisplayInterpreter, totalStepCount};
