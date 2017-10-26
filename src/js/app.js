b37ce535e8660d8f17f5ee8ba370597e8e521639
Xiaoru Li <thexrcs@gmail.com>
2017-10-26 13:36:47 +0200

    Add mention of TI Website to source code

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,18 @@
+/*
+ MIMAviz - A modern browser friendly MIMA visualization
+
+ MIMA (Minimalmachine) is a simple processor model based on von Neumann architecture, used for
+ educational purposes at the KIT. It is developed by Prof. Tamim Asfour.
+
+ This is a rewrite project of the MIMA simulation (http://ti.ira.uka.de/Visualisierungen/Mima)
+ in Javascript and friends, created by Xiaoru Li and distributed under MIT license (see LICENSE).
+
+ The German descriptions in this visualization were largely borrowed from the original MIMA
+ simulation, created by Thorsten Rapp and supervised by Prof. Tamim Asfour.
+ */
+
 import * as PIXI from 'pixi.js';
 import * as store from './store';
-import * as view from './view';
 
 export const app = new PIXI.Application({antialias: true, transparent: true});
 document.getElementById('mimaviz').appendChild(app.view);
